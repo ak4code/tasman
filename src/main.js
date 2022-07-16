055fa8ef904701ada8a9e95779aeeb9d17b2626c
ak4code <haritonov.aka@gmail.com>
2022-07-16 22:03:19 +0300

    add axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import axios from "./plugins/axios"
 import { createApp } from "vue"
 // import './registerServiceWorker'
 import store from "./store"
 
 const app = createApp({})
-app.use(store).mount("#app")
+app.use(store).use(axios).mount("#tasman")
