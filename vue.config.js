081e1f0dc3269cd2e24a8dd8f91210cccfb35f27
ak4code <haritonov.aka@gmail.com>
2022-07-16 22:16:44 +0300

    setting manifestloader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,39 @@
 const { defineConfig } = require('@vue/cli-service')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  css: {
+    extract: true
+  },
+  filenameHashing: false,
+  chainWebpack: config => {
+    config.plugins.delete("html")
+    config.plugins.delete("preload")
+    config.plugins.delete("prefetch")
+  },
+  configureWebpack: {
+    plugins: [
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new WebpackManifestPlugin({})
+    ]
+  },
+  devServer: {
+    port: 8080,
+    allowedHosts: [
+      "127.0.0.1:8000",
+      "127.0.0.1"
+    ],
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
+  transpileDependencies: true,
+  runtimeCompiler: true,
 })
